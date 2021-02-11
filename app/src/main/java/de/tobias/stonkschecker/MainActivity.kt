a73a2a9f614e22c77916a4fccd47e2c11d08858e
tobias <tobias.kamm@pm.me>
2021-02-11 19:10:25 +0100

    Corrected MainActivity.kt

--- app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
+++ app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
@@ -1,11 +1,11 @@
 package de.tobias.stonkschecker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) s
+        setContentView(R.layout.activity_main)
     }
 }
