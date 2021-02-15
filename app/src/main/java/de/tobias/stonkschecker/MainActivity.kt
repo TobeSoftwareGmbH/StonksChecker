65bf927bf98f26e2d75d7afeba4b3fdf24f2c2fb
tobias <tobias.kamm@pm.me>
2021-02-15 11:01:47 +0100

    Made some functions "static"

--- app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
+++ app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
@@ -2,14 +2,10 @@ package de.tobias.stonkschecker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import de.tobias.stonkschecker.network.NetworkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val stocksManager = NetworkManager(this)
-        stocksManager.getJSONResponse(stocksManager.getStockURL("GME:US"))
     }
 }
--- app/src/main/java/de/tobias/stonkschecker/network/NetworkManager.kt
+++ app/src/main/java/de/tobias/stonkschecker/network/NetworkManager.kt
@@ -14,12 +14,14 @@ class NetworkManager(context: Context) {
         queue = Volley.newRequestQueue(context)
     }
 
-    fun getSearchURL(query: String) : String {
-        return "https://search.bloomberg.com/lookup.json?types=Company_Public,Index,Fund,Currency,Commodity,Bond,Person,Author,Topic&exclude_subtypes=label:editorial&group_size=3,3,3,3,3,3,3,3,6&fields=name,slug,ticker_symbol,url,organization,title,primary_site&highlight=1&query=$query"
-    }
+    companion object {
+        fun getSearchURL(query: String): String {
+            return "https://search.bloomberg.com/lookup.json?types=Company_Public,Index,Fund,Currency,Commodity,Bond,Person,Author,Topic&exclude_subtypes=label:editorial&group_size=3,3,3,3,3,3,3,3,6&fields=name,slug,ticker_symbol,url,organization,title,primary_site&highlight=1&query=$query"
+        }
 
-    fun getStockURL(stockName: String) : String {
-        return "https://www.bloomberg.com/markets/api/bulk-time-series/price/$stockName?timeFrame=1_DAY"
+        fun getStockURL(stockName: String): String {
+            return "https://www.bloomberg.com/markets/api/bulk-time-series/price/$stockName?timeFrame=1_DAY"
+        }
     }
 
     fun getJSONResponse(url: String, networkCallback: NetworkCallback): NetworkCallback {
