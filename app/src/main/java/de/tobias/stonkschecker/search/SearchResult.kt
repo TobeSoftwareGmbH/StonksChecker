1f226b8246ac9f2fcc302cd24645456355734856
tobias <tobias.kamm@pm.me>
2021-02-17 20:06:03 +0100

    Added current Stock value and and ArrayList with just SearchResult.kt objects

--- app/src/main/java/de/tobias/stonkschecker/search/SearchResult.kt
+++ app/src/main/java/de/tobias/stonkschecker/search/SearchResult.kt
@@ -1,13 +1,13 @@
 package de.tobias.stonkschecker.search
 
-class SearchResult (val name: String, val ticker_symbol: String){
+class SearchResult (val name: String, val ticker_symbol: String, val value: Double){
 
     companion object {
-        fun fromValues(name: String, ticker_symbol: String) : SearchResult {
-            return SearchResult(removeEMTags(name), removeEMTags(ticker_symbol))
+        fun fromValues(name: String, ticker_symbol: String, value: Double) : SearchResult {
+            return SearchResult(removeEMTags(name), removeEMTags(ticker_symbol), value)
         }
 
-        fun removeEMTags(string: String) : String {
+        private fun removeEMTags(string: String) : String {
             return string.replace("<em>", "").replace("</em>", "")
         }
     }
