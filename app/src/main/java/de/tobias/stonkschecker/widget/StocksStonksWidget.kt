6f463549f3338a32e565da3b634679da9f307eea
tobias <tobias.kamm@pm.me>
2021-02-18 18:54:49 +0100

    The update button is now functional

--- app/src/main/java/de/tobias/stonkschecker/widget/StocksStonksWidget.kt
+++ app/src/main/java/de/tobias/stonkschecker/widget/StocksStonksWidget.kt
@@ -1,14 +1,29 @@
 package de.tobias.stonkschecker.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.view.View
+import android.widget.RemoteViews
+import com.android.volley.VolleyError
+import de.tobias.stonkschecker.R
+import de.tobias.stonkschecker.network.NetworkCallback
+import de.tobias.stonkschecker.network.NetworkManager
+import de.tobias.stonkschecker.stocks.Stock
+import org.json.JSONArray
+
 
 /**
  * Implementation of App Widget functionality.
  * App Widget Configuration implemented in [StocksStonksWidgetConfigureActivity]
  */
-class StocksStonksWidget : AppWidgetProvider() {
+class StocksStonksWidget : NetworkCallback, AppWidgetProvider() {
+    private lateinit var context: Context
+    private lateinit var appWidgetManager: AppWidgetManager
+    private var appWidgetId = 0 //lateinit is not allowed, initialising with random value
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -23,7 +38,7 @@ class StocksStonksWidget : AppWidgetProvider() {
     override fun onDeleted(context: Context, appWidgetIds: IntArray) {
         // When the user deletes the widget, delete the preference associated with it.
         for (appWidgetId in appWidgetIds) {
-            deleteStockName(context, appWidgetId)
+            deleteStockData(context, appWidgetId)
         }
     }
 
@@ -34,18 +49,73 @@ class StocksStonksWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    //val widgetText = loadTitlePref(context, appWidgetId)
-    // Construct the RemoteViews object
-    //val views = RemoteViews(context.packageName, R.layout.stocks_stonks_widget)
-    //views.setTextViewText(R.id.appwidget_text, widgetText)
+    override fun onFinished(jsonArray: JSONArray) {
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.stocks_stonks_widget)
 
-    // Instruct the widget manager to update the widget
-    //appWidgetManager.updateAppWidget(appWidgetId, views)
+        val stock: Stock = Stock.getStockData(jsonArray)
+        val stonkState: Int = stock.getStonkState()
+
+        views.setViewVisibility(R.id.widget_loading, View.GONE)
+        views.setTextViewText(R.id.stock_name, getStockName(context, appWidgetId) + ":")
+
+        if(stonkState == Stock.STONKS) {
+            views.setImageViewResource(R.id.img_stonks, R.drawable.stonks)
+        } else if (stonkState == Stock.NOT_STONKS) {
+            views.setImageViewResource(R.id.img_stonks, R.drawable.not_stonks)
+        } else {
+            views.setImageViewResource(R.id.img_stonks, R.drawable.confused_stonks)
+        }
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    override fun onError(error: VolleyError) {
+        TODO("Not yet implemented")
+    }
+
+    private fun getPendingSelfIntent(context: Context, appWidgetId: Int): PendingIntent? {
+        val intent = Intent(context, StocksStonksWidget::class.java)
+        intent.action = "StonkReload"
+        intent.putExtra("widgetId", appWidgetId)
+        return PendingIntent.getBroadcast(context, 0, intent, 0)
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if(intent != null && context != null && intent.action.equals("StonkReload")) {
+            updateAppWidget(
+                context, AppWidgetManager.getInstance(context), intent.getIntExtra(
+                    "widgetId",
+                    0
+                )
+            )
+        }
+    }
+
+    internal fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        this.context = context
+        this.appWidgetManager = appWidgetManager
+        this.appWidgetId = appWidgetId
+
+        if(!isInitialised(context, appWidgetId)) return //This function is also called when the user adds the widget for the first time. At that stage, the request is ignored
+
+        val views = RemoteViews(context.packageName, R.layout.stocks_stonks_widget)
+        views.setViewVisibility(R.id.widget_loading, View.VISIBLE)
+        views.setOnClickPendingIntent(
+            R.id.img_reload,
+            getPendingSelfIntent(context, appWidgetId)
+        );
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+
+        val tickerName = getTickerSymbol(context, appWidgetId)
+        val networkManager = NetworkManager(context)
+        networkManager.getJSONResponse(NetworkManager.getStockURL(tickerName), this)
+    }
 }
