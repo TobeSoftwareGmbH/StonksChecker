738e0b88db67042049895ee7ece7d3b849c43c2f
tobias <tobias.kamm@pm.me>
2021-02-21 13:09:16 +0100

    Hiding the "No widgets"-container if the user has widgets on their home-screen

--- app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
+++ app/src/main/java/de/tobias/stonkschecker/MainActivity.kt
@@ -1,7 +1,9 @@
 package de.tobias.stonkschecker
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import de.tobias.stonkschecker.widget.WidgetManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        //Get all widget ids
+        val widgetIds = WidgetManager.getWidgetIds(this)
+        if (widgetIds.isEmpty()) { //No widgets added, display a message to the user
+            findViewById<View>(R.id.container_no_widgets).visibility = View.VISIBLE
+            findViewById<View>(R.id.container_widget_overview).visibility = View.GONE
+        } else { //Widgets added, show an overview to the user
+            findViewById<View>(R.id.container_no_widgets).visibility = View.GONE
+            findViewById<View>(R.id.container_widget_overview).visibility = View.VISIBLE
+        }
+    }
 
 }
